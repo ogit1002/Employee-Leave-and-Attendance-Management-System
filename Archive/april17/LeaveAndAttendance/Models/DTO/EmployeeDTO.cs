39d91923e13e55e7af62d1b59817c2b8d06b7c2e
Omkar Bajaga <141669528+ogit1002@users.noreply.github.com>
2025-08-26 22:26:40 +0530

    Delete Archive/april17 directory

--- Archive/april17/LeaveAndAttendance/Models/DTO/EmployeeDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LeaveAndAttendance.Models.DTO
-{
-    public class EmployeeDTO
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-    }
-}
